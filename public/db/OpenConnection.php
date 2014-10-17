e5bb6e271c1b18c9e8b3f1a19e31997f09ae6c6a
Joseph Whitaker <joseph.whitaker@forrent.com>
2014-10-17 13:02:55 -0400

    update to database connection

--- public/db/OpenConnection.php
+++ public/db/OpenConnection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '','csv_db');
+$conn = mysqli_connect('104.131.94.62', 'root', 'NFZe6vlN9y','csv_db');
 if (!$conn) {
     die('Could not connect: ' . mysqli_error());
 }
